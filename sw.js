fa82745b5140f5a81667614a05e5bd354d232a7b
crisbernalji <kristinabernal2009@gmail.com>
2026-08-20 12:02:25 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE='my-day-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
-self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.matchAll({type:'window',includeUncontrolled:true}).then(cs=>cs.length?cs[0].focus():clients.openWindow('./')))});
-self.addEventListener('push',e=>{let d={title:'My Day',body:'Tienes un recordatorio ✦',url:'./'};try{d={...d,...e.data.json()}}catch(_){}e.waitUntil(self.registration.showNotification(d.title,{body:d.body,icon:'./icons/icon-192.png',badge:'./icons/icon-192.png',data:{url:d.url}}))});
