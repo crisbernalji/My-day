ac7066e196f87480bd1b41b37bc18de6cb9f3e84
crisbernalji <kristinabernal2009@gmail.com>
2026-08-20 14:23:05 +0200

    Update cache version from 'my-day-v1' to 'my-day-v2'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='my-day-v1';
+const CACHE='my-day-v2';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
